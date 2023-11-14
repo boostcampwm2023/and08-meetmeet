bed2b839b6636c10f9afa1af769b06e6da7f30a7
p-chanmin <pc_m111@naver.com>
2023-11-14 15:33:17 +0900

    build: Dependency 주석 추가 (#63)
    Co-authored-by: p-chanmin <pc_m111@naver.com>
    Co-authored-by: agfalcon <agfalcon12@gmail.com>
    Co-authored-by: LeeHaiLim <hailim1997@gmail.com>

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,27 +48,39 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Test
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // Activity, Fragment ktx 확장 기능
     implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
+
+    // Navigation
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
 
+    // Di Hilt
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-android-compiler:2.48")
 
+    // Moshi JSON
     implementation("com.squareup.moshi:moshi:1.14.0")
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.14.0")
 
+    // 코루틴
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
 
+    // Datastore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
+    // Room DB
     val room_version = "2.6.0"
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
