e64c722445c240b2a2fb517084cec3f295079d98
p-chanmin <pc_m111@naver.com>
2023-11-14 18:49:46 +0900

    bug: binding getter설정 (#63)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/base/BaseFragment.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/base/BaseFragment.kt
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment
 abstract class BaseFragment<T : ViewDataBinding>(private val layoutResId: Int) : Fragment() {
 
     private var _binding: T? = null
-    protected val binding = requireNotNull(_binding)
+    protected val binding get() = requireNotNull(_binding)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
