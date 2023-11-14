8a7b237e571b97e1257a4d42d052c4b6ce4a810c
p-chanmin <pc_m111@naver.com>
2023-11-14 15:32:03 +0900

    build: retrofit2 dependency 추가 (#63)
    Co-authored-by: p-chanmin <pc_m111@naver.com>
    Co-authored-by: agfalcon <agfalcon12@gmail.com>
    Co-authored-by: LeeHaiLim <hailim1997@gmail.com>

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -64,4 +64,7 @@ dependencies {
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.14.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+
 }
