725835087d3c3982c3dfd9267f379ec3089aa53b
p-chanmin <pc_m111@naver.com>
2023-11-14 15:33:53 +0900

    feat: Base Activity, Fragment 추가 (#63)
    Co-authored-by: p-chanmin <pc_m111@naver.com>
    Co-authored-by: agfalcon <agfalcon12@gmail.com>
    Co-authored-by: LeeHaiLim <hailim1997@gmail.com>

---
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/base/BaseFragment.kt
@@ -0,0 +1,30 @@
+package com.teameetmeet.meetmeet.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : ViewDataBinding>(private val layoutResId: Int) : Fragment() {
+
+    private var _binding: T? = null
+    val binding = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
