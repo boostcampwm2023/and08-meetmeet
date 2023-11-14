7591cbef4e585f43ee33d6596acf02c68202f942
agfalcon <agfalcon12@gmail.com>
2023-11-14 17:51:25 +0900

    chore: Calendar Fragment 생성 (#31)
    - CalendarFragment 생성 및 BaseFragment 상속
    - fragment_calendar layout 생성 및 데이터 바인딩

---
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/calendar/CalendarFragment.kt
@@ -0,0 +1,9 @@
+package com.teameetmeet.meetmeet.presentation.calendar
+
+import com.teameetmeet.meetmeet.R
+import com.teameetmeet.meetmeet.databinding.FragmentCalendarBinding
+import com.teameetmeet.meetmeet.presentation.base.BaseFragment
+
+class CalendarFragment() : BaseFragment<FragmentCalendarBinding>(R.layout.fragment_calendar) {
+
+}
