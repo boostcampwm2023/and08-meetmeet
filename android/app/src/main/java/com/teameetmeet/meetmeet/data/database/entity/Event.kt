6f9ad9bdf3b78dc6a721a88d8e4589e4a90ee009
LeeHaiLim <hailim1997@gmail.com>
2023-11-15 05:32:19 +0900

    feat: 로컬 DB 일정 데이터 엔티티 작성 (#15) - 룸 엔티티 작성

---
+++ android/app/src/main/java/com/teameetmeet/meetmeet/data/database/entity/Event.kt
@@ -0,0 +1,15 @@
+package com.teameetmeet.meetmeet.data.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Event(
+    @PrimaryKey
+    val id: Int,
+    val title: String,
+    val startDateTime: Long,
+    val endDateTime: Long,
+    val notification: String = "none",
+    val color: String = "#FF6565",
+)
