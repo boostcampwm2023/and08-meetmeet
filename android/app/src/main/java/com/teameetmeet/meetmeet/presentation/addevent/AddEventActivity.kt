c1f4dac39576509ca6eb749cc358f522e466e1f3
p-chanmin <pc_m111@naver.com>
2023-11-15 03:06:53 +0900

    feat: 일정 제목, 메모 UI 기능 연결 (#28)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/addevent/AddEventActivity.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/addevent/AddEventActivity.kt
@@ -1,6 +1,8 @@
 package com.teameetmeet.meetmeet.presentation.addevent
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.viewModels
 import com.teameetmeet.meetmeet.R
 import com.teameetmeet.meetmeet.databinding.ActivityAddEventBinding
@@ -18,6 +20,7 @@ class AddEventActivity : BaseActivity<ActivityAddEventBinding>(R.layout.activity
         binding.vm = viewModel
 
         setTopAppBar()
+        setTextChangeListener()
 
     private fun setTopAppBar() {
         binding.topAppBar.setOnMenuItemClickListener { menuItem ->
@@ -30,6 +33,36 @@ class AddEventActivity : BaseActivity<ActivityAddEventBinding>(R.layout.activity
                 else -> false
             }
         }
+    }
+
+    private fun setTextChangeListener() {
+        binding.etEventName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.setEventName(binding.etEventName.text.toString())
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                binding.etEventName.text?.let { name ->
+                    if (name.isEmpty()) {
+                        binding.tfEventName.error = "일정 이름을 입력해 주세요."
+                    } else {
+                        binding.tfEventName.error = null
+                    }
+                }
+            }
+        })
+
+        binding.etEventMemo.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.setEventMemo(binding.etEventMemo.text.toString())
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
     }
     }
 }
