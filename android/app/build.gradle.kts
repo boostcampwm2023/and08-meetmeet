3d2e567942e514281e36003d08c54ae1b18bae3d
p-chanmin <pc_m111@naver.com>
2023-11-14 15:30:49 +0900

    build: coroutines dependency 추가 (#63)
    Co-authored-by: p-chanmin <pc_m111@naver.com>
    Co-authored-by: agfalcon <agfalcon12@gmail.com>
    Co-authored-by: LeeHaiLim <hailim1997@gmail.com>

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -60,4 +60,5 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-android-compiler:2.48")
 
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
