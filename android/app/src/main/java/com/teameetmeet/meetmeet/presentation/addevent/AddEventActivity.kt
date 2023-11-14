cc307109553bc902566350bdcb088305cf7b8d2e
p-chanmin <pc_m111@naver.com>
2023-11-15 03:09:15 +0900

    feat: 일정 기간 설정 UI 기능 연결 (#28)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/addevent/AddEventActivity.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/addevent/AddEventActivity.kt
@@ -1,9 +1,14 @@
 package com.teameetmeet.meetmeet.presentation.addevent
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.MotionEvent
 import androidx.activity.viewModels
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.teameetmeet.meetmeet.R
 import com.teameetmeet.meetmeet.databinding.ActivityAddEventBinding
 import com.teameetmeet.meetmeet.presentation.base.BaseActivity
@@ -21,6 +26,8 @@ class AddEventActivity : BaseActivity<ActivityAddEventBinding>(R.layout.activity
 
         setTopAppBar()
         setTextChangeListener()
+        setDatePicker()
+        setTimePicker()
 
     private fun setTopAppBar() {
         binding.topAppBar.setOnMenuItemClickListener { menuItem ->
@@ -63,6 +70,66 @@ class AddEventActivity : BaseActivity<ActivityAddEventBinding>(R.layout.activity
 
             override fun afterTextChanged(p0: Editable?) {}
         })
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setDatePicker() {
+        val datePicker =
+            MaterialDatePicker.Builder.dateRangePicker().setTitleText(getString(R.string.add_event))
+                .setSelection(
+                    viewModel.eventDate.value
+                ).build()
+
+        datePicker.addOnPositiveButtonClickListener {
+            viewModel.setEventDate(it.first, it.second)
+        }
+
+        binding.etEventStartDate.setOnTouchListener { _, e ->
+            if (e.action == MotionEvent.ACTION_UP) {
+                datePicker.show(supportFragmentManager, "DatePicker")
+            }
+            true
+        }
+        binding.etEventEndDate.setOnTouchListener { _, e ->
+            if (e.action == MotionEvent.ACTION_UP) {
+                datePicker.show(supportFragmentManager, "DatePicker")
+            }
+            true
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setTimePicker() {
+        val startTimePicker = MaterialTimePicker.Builder().setTimeFormat(TimeFormat.CLOCK_12H)
+            .setHour(viewModel.eventStartTime.value.hour)
+            .setMinute(viewModel.eventStartTime.value.minute).setTitleText("시작 시간을 지정해 주세요.")
+            .build()
+
+        val endTimePicker = MaterialTimePicker.Builder().setTimeFormat(TimeFormat.CLOCK_12H)
+            .setHour(viewModel.eventEndTime.value.hour)
+            .setMinute(viewModel.eventEndTime.value.minute).setTitleText("종료 시간을 지정해 주세요.").build()
+
+        binding.etEventStartTime.setOnTouchListener { _, e ->
+            if (e.action == MotionEvent.ACTION_UP) {
+                startTimePicker.show(supportFragmentManager, "StartTimePicker")
+            }
+            true
+        }
+
+        startTimePicker.addOnPositiveButtonClickListener {
+            viewModel.setEventStartTime(startTimePicker.hour, startTimePicker.minute)
+        }
+
+        binding.etEventEndTime.setOnTouchListener { _, e ->
+            if (e.action == MotionEvent.ACTION_UP) {
+                endTimePicker.show(supportFragmentManager, "EndTimePicker")
+            }
+            true
+        }
+
+        endTimePicker.addOnPositiveButtonClickListener {
+            viewModel.setEventEndTime(endTimePicker.hour, endTimePicker.minute)
+        }
     }
     }
 }
