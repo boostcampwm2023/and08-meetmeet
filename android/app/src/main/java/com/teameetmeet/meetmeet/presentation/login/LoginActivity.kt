9ab5c23d44a4e9b7a05fd5073b4ba2f3ac42d79b
agfalcon <agfalcon12@gmail.com>
2023-11-15 14:24:50 +0900

    chore: ãLoginActivity 생성 (#38)

---
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/login/LoginActivity.kt
@@ -0,0 +1,9 @@
+package com.teameetmeet.meetmeet.presentation.login
+
+import com.teameetmeet.meetmeet.R
+import com.teameetmeet.meetmeet.databinding.ActivityLoginBinding
+import com.teameetmeet.meetmeet.presentation.base.BaseActivity
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+
+}
