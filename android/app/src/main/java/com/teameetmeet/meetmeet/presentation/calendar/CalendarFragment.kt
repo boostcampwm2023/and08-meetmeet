f2df14d3045457d85f32fa9dca45124362d8a950
p-chanmin <pc_m111@naver.com>
2023-11-15 14:54:18 +0900

    feat: 일정 추가 Activity action 연결 (#51)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/calendar/CalendarFragment.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/calendar/CalendarFragment.kt
@@ -3,10 +3,13 @@ package com.teameetmeet.meetmeet.presentation.calendar
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.teameetmeet.meetmeet.R
 import com.teameetmeet.meetmeet.databinding.FragmentCalendarBinding
 import com.teameetmeet.meetmeet.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CalendarFragment : BaseFragment<FragmentCalendarBinding>(R.layout.fragment_calendar) {
 
     private val viewModel: CalendarViewModel by viewModels()
@@ -14,5 +17,9 @@ class CalendarFragment : BaseFragment<FragmentCalendarBinding>(R.layout.fragment
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.vm = viewModel
+
+        binding.fabAddEvent.setOnClickListener {
+            findNavController().navigate(CalendarFragmentDirections.actionCalendarFragmentToAddEventActivity())
+        }
     }
 }
