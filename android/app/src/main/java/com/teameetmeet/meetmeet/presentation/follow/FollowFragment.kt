579fc97ad689aea8ead4993abfcf6b1834c7a1ec
p-chanmin <pc_m111@naver.com>
2023-11-15 14:53:22 +0900

    feat: 팔로우, 그룹 Fragment 추가 (#51)

---
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/follow/FollowFragment.kt
@@ -0,0 +1,16 @@
+package com.teameetmeet.meetmeet.presentation.follow
+
+import android.os.Bundle
+import android.view.View
+import com.teameetmeet.meetmeet.R
+import com.teameetmeet.meetmeet.databinding.FragmentFollowBinding
+import com.teameetmeet.meetmeet.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FollowFragment : BaseFragment<FragmentFollowBinding>(R.layout.fragment_follow) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/group/GroupFragment.kt
@@ -0,0 +1,16 @@
+package com.teameetmeet.meetmeet.presentation.group
+
+import android.os.Bundle
+import android.view.View
+import com.teameetmeet.meetmeet.R
+import com.teameetmeet.meetmeet.databinding.FragmentGroupBinding
+import com.teameetmeet.meetmeet.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class GroupFragment : BaseFragment<FragmentGroupBinding>(R.layout.fragment_group) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
