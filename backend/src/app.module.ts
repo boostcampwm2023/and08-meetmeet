f6aad9ae475327bcd3f938a1e0dd96253fb3120e
cdj2073 <daioww60@gmail.com>
2023-11-15 22:17:40 +0900

    chore: restore missing files (#28)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CalendarController } from './calendar/calendar.controller';
+import { CalendarService } from './calendar/calendar.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CalendarController],
+  providers: [AppService, CalendarService],
 })
 export class AppModule {}
