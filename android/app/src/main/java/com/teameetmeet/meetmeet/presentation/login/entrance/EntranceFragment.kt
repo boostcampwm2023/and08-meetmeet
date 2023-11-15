b51a5e91ba338b2f0f78530d4037cdb49a32f460
p-chanmin <pc_m111@naver.com>
2023-11-16 01:05:53 +0900

    feat: 자체 로그인 Fragment 이동 기능 연결 (#47)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/login/entrance/EntranceFragment.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/login/entrance/EntranceFragment.kt
@@ -2,11 +2,13 @@ package com.teameetmeet.meetmeet.presentation.login.entrance
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.teameetmeet.meetmeet.R
 import com.teameetmeet.meetmeet.databinding.FragmentEntranceBinding
 import com.teameetmeet.meetmeet.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class EntranceFragment : BaseFragment<FragmentEntranceBinding>(R.layout.fragment_entrance) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -20,7 +22,9 @@ class EntranceFragment : BaseFragment<FragmentEntranceBinding>(R.layout.fragment
         }
 
         binding.loginTvAppLogin.setOnClickListener {
-
+            findNavController().navigate(
+                EntranceFragmentDirections.actionEntranceFragmentToSelfLoginFragment()
+            )
         }
 
         binding.loginTvAppSignUp.setOnClickListener {
