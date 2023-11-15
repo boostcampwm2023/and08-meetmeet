c78badc4dae9f0dcf49cfd38797e6d126f8c78cb
LeeHaiLim <hailim1997@gmail.com>
2023-11-16 05:03:52 +0900

    feat: date picker 띄우기 (#34) - 구간 설정에 따라 date picker 기본 구간 설정 변경

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/searchevent/SearchEventActivity.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/searchevent/SearchEventActivity.kt
@@ -2,9 +2,13 @@ package com.teameetmeet.meetmeet.presentation.searchevent
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.teameetmeet.meetmeet.R
 import com.teameetmeet.meetmeet.databinding.ActivitySearchEventBinding
 import com.teameetmeet.meetmeet.presentation.base.BaseActivity
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class SearchActivity : BaseActivity<ActivitySearchEventBinding>(
     R.layout.activity_search_event
@@ -19,6 +23,27 @@ class SearchActivity : BaseActivity<ActivitySearchEventBinding>(
             lifecycleOwner = this@SearchActivity
             searchEventSv.setupWithSearchBar(searchEventSb)
             searchEventSpinner.setSelection(1)
+        }
+    }
+
+    private fun setDatePicker() {
+        val datePickerBuilder =
+            MaterialDatePicker.Builder.dateRangePicker()
+                .setTitleText(getString(R.string.search_event_title))
+
+        binding.searchEventTvDateRange.setOnClickListener {
+            datePickerBuilder.build().apply {
+                this.addOnPositiveButtonClickListener {
+                    viewModel.setSearchDateRange(it)
+                    binding.searchEventSpinner.setSelection(0)
+                }
+            }.show(supportFragmentManager, "datePicker")
+        }
+
+        lifecycleScope.launch {
+            viewModel.searchDateRange.collectLatest {
+                datePickerBuilder.setSelection(it)
+            }
         }
     }
 }
