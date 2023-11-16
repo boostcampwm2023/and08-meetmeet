f08805aff520e5760ca63638fec7eeb3ff7b3fa6
p-chanmin <pc_m111@naver.com>
2023-11-17 01:06:28 +0900

    refactor: 로그인 UiState 변경 (#47)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/login/selflogin/SelfLoginViewModel.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/login/selflogin/SelfLoginViewModel.kt
@@ -3,10 +3,13 @@ package com.teameetmeet.meetmeet.presentation.login.selflogin
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.teameetmeet.meetmeet.data.repository.UserRepository
+import com.teameetmeet.meetmeet.presentation.model.EmailState
+import com.teameetmeet.meetmeet.presentation.model.PasswordState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.update
@@ -18,31 +21,34 @@ class SelfLoginViewModel @Inject constructor(
     private val userRepository: UserRepository
 ) : ViewModel() {
 
-    private val _email: MutableStateFlow<String> = MutableStateFlow("")
-
-    private val _password: MutableStateFlow<String> = MutableStateFlow("")
+    private val _uiState: MutableStateFlow<SelfLoginUiState> = MutableStateFlow(SelfLoginUiState())
+    val uiState: StateFlow<SelfLoginUiState> = _uiState
 
     private val _event: MutableSharedFlow<SelfLoginEvent> = MutableSharedFlow()
     val event: SharedFlow<SelfLoginEvent> = _event
 
     fun setEmail(email: CharSequence?) {
-        _email.update { email.toString() }
+        val emailString = email.toString()
+        val state = if (emailString.isNotEmpty()) EmailState.Valid else EmailState.None
+        _uiState.update { it.copy(email = email.toString(), emailState = state) }
     }
 
     fun setPassword(password: CharSequence?) {
-        _password.update { password.toString() }
+        val passwordString = password.toString()
+        val state = if (passwordString.isNotEmpty()) PasswordState.Valid else PasswordState.None
+        _uiState.update { it.copy(password = password.toString(), passwordState = state) }
     }
 
     fun login() {
         // todo 로그인 API 호출
         println(
             """
-            email : ${_email.value}
-            password : ${_password.value}
+            email : ${_uiState.value.email}
+            password : ${_uiState.value.password}
         """.trimIndent()
         )
         viewModelScope.launch {
-            userRepository.login(_email.value, _password.value)
+            userRepository.login(_uiState.value.email, _uiState.value.password)
                 .catch {
                     // 예외 처리
                 }.collectLatest {
