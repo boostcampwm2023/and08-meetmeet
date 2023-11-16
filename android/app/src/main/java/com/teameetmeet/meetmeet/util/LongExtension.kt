66165dbda8ee9d3b6dbd62497be373bfe13f2bc7
LeeHaiLim <hailim1997@gmail.com>
2023-11-16 17:46:04 +0900

    feat: 확장 함수 확장성 있게 변경 (#34)

--- android/app/src/main/java/com/teameetmeet/meetmeet/util/LongExtension.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/util/LongExtension.kt
@@ -4,7 +4,7 @@ import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneId
 
-fun Long.toLocalDate(): LocalDate {
-    return Instant.ofEpochMilli(this).atZone(ZoneId.systemDefault()).toLocalDate()
+fun Long.toLocalDate(zoneId: ZoneId): LocalDate {
+    return Instant.ofEpochMilli(this).atZone(zoneId).toLocalDate()
 }
 
