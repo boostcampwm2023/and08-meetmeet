b195fc429edc103e0705205db01fb3d4512f0397
LeeHaiLim <hailim1997@gmail.com>
2023-11-16 19:33:53 +0900

    feat: 캘린더 프래그먼트에서 이벤트 수집(#36) - 일 클릭 시 네비게이션을 통해 다이얼로그 보여주기

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/calendar/CalendarFragment.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/calendar/CalendarFragment.kt
@@ -3,11 +3,15 @@ package com.teameetmeet.meetmeet.presentation.calendar
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.teameetmeet.meetmeet.R
 import com.teameetmeet.meetmeet.databinding.FragmentCalendarBinding
 import com.teameetmeet.meetmeet.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CalendarFragment : BaseFragment<FragmentCalendarBinding>(R.layout.fragment_calendar) {
@@ -18,6 +22,7 @@ class CalendarFragment : BaseFragment<FragmentCalendarBinding>(R.layout.fragment
         super.onViewCreated(view, savedInstanceState)
         binding.vm = viewModel
         setClickListener()
+        collectViewModelEvent()
     }
 
     private fun setClickListener() {
@@ -27,6 +32,16 @@ class CalendarFragment : BaseFragment<FragmentCalendarBinding>(R.layout.fragment
 
         binding.calendarClProfile.setOnClickListener {
             findNavController().navigate(CalendarFragmentDirections.actionCalendarFragmentToSettingActivity())
+        }
+    }
+
+    private fun collectViewModelEvent() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.dayClickEvent.collect {
+                    findNavController().navigate(CalendarFragmentDirections.actionCalendarFragmentToBottomSheet())
+                }
+            }
         }
     }
 }
