736c666593ab3e12cb04ba74f52bfd0b853d5716
cdj2073 <daioww60@gmail.com>
2023-11-16 16:35:10 +0900

    feat: database 연결 (#63)
    기존에 작성했던 코드를 제거하고 db 연결하여 findAll 테스트
    EventEntity도 테스트용으로 작성하여 추후 수정 예정

--- backend/src/calendar/calendar.module.ts
+++ backend/src/calendar/calendar.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CalendarController } from './calendar.controller';
 import { CalendarService } from './calendar.service';
+import { EventEntity } from './event.entity';
 
-@Module({ controllers: [CalendarController], providers: [CalendarService] })
+@Module({
+  imports: [TypeOrmModule.forFeature([EventEntity])],
+  controllers: [CalendarController],
+  providers: [CalendarService],
+})
 export class CalendarModule {}
