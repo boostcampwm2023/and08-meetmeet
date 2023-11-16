798f572fef81ff34d0bf1b026fa4425f8627c3d3
LeeHaiLim <hailim1997@gmail.com>
2023-11-16 23:35:02 +0900

    feat: api endpoint 추가 (#15)

--- android/app/src/main/java/com/teameetmeet/meetmeet/data/network/api/CalendarApi.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/data/network/api/CalendarApi.kt
@@ -4,8 +4,8 @@ import com.teameetmeet.meetmeet.data.network.entity.EventResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface ServerApi {
-    @GET //todo: end point
+interface CalendarApi {
+    @GET("calendar")
     fun getEvents(
         @Query("startDate") startDate: String,
         @Query("endDate") endDate: String,
