c2e9541ab9812d03e1b65a112e7e9b4f5e309374
p-chanmin <pc_m111@naver.com>
2023-11-17 02:35:30 +0900

    feat: 회원가입 API 호출 로직 추가 (#50)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/login/signup/SignUpFragment.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/login/signup/SignUpFragment.kt
@@ -3,11 +3,14 @@ package com.teameetmeet.meetmeet.presentation.login.signup
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.teameetmeet.meetmeet.R
 import com.teameetmeet.meetmeet.databinding.FragmentSignUpBinding
 import com.teameetmeet.meetmeet.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sign_up) {
@@ -20,11 +23,27 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sig
         binding.vm = viewModel
 
         setTopAppBar()
+        collectViewModelEvent()
     }
 
     private fun setTopAppBar() {
         binding.topAppBar.setNavigationOnClickListener {
             findNavController().popBackStack()
+        }
+    }
+
+    private fun collectViewModelEvent() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.event.collectLatest { event ->
+                when (event) {
+                    SignUpEvent.SignUpSuccess -> {
+                        findNavController().navigate(
+                            SignUpFragmentDirections.actionSignUpFragmentToHomeActivity()
+                        )
+                        requireActivity().finish()
+                    }
+                }
+            }
         }
     }
 }
--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/login/signup/SignUpViewModel.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/login/signup/SignUpViewModel.kt
@@ -1,20 +1,32 @@
 package com.teameetmeet.meetmeet.presentation.login.signup
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.teameetmeet.meetmeet.data.repository.UserRepository
 import com.teameetmeet.meetmeet.presentation.model.EmailState
 import com.teameetmeet.meetmeet.presentation.model.PasswordState
 import com.teameetmeet.meetmeet.util.Verification
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SignUpViewModel @Inject constructor() : ViewModel() {
+class SignUpViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
 
     private val _uiState: MutableStateFlow<SignUpUiState> = MutableStateFlow(SignUpUiState())
     val uiState: StateFlow<SignUpUiState> = _uiState
+
+    private val _event: MutableSharedFlow<SignUpEvent> = MutableSharedFlow()
+    val event: SharedFlow<SignUpEvent> = _event
 
     fun setEmail(email: CharSequence?) {
         val emailString = email.toString()
@@ -52,7 +64,14 @@ class SignUpViewModel @Inject constructor() : ViewModel() {
 
     fun checkDuplicate() {
         // todo email 중복 확인 API 호출
-        _uiState.update { it.copy(emailDuplicateCheck = true) }
+        viewModelScope.launch {
+            userRepository.checkEmailDuplicate(_uiState.value.email)
+                .catch {
+                    // 예외 처리
+                }.collectLatest { result ->
+                    _uiState.update { it.copy(emailDuplicateCheck = result) }
+                }
+        }
     }
 
     fun signUp() {
@@ -64,6 +83,15 @@ class SignUpViewModel @Inject constructor() : ViewModel() {
             passwordConfirm : ${_uiState.value.passwordConfirm}
         """.trimIndent()
         )
+        viewModelScope.launch {
+            userRepository.signUp(_uiState.value.email, _uiState.value.password)
+                .catch {
+                    // 예외 처리
+                }
+                .collectLatest {
+                    _event.emit(SignUpEvent.SignUpSuccess)
+                }
+        }
     }
 
     private fun getEmailState(email: String): EmailState {
