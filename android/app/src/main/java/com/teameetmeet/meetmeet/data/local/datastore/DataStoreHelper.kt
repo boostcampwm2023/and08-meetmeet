7147a505c78971219c85729da04eff42960b61ac
agfalcon <agfalcon12@gmail.com>
2023-11-18 02:31:57 +0900

    feat: ì 캘린더 입장 시 유저 정보 ë관련 데이터 스토어 부ë저장 및 호출 기능 구현 (#45)

--- android/app/src/main/java/com/teameetmeet/meetmeet/data/local/datastore/DataStoreHelper.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/data/local/datastore/DataStoreHelper.kt
@@ -4,8 +4,10 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import com.teameetmeet.meetmeet.data.repository.UserRepository
+import com.teameetmeet.meetmeet.data.NoDataException
+import com.teameetmeet.meetmeet.data.network.entity.UserProfile
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
@@ -14,19 +16,37 @@ class DataStoreHelper @Inject constructor(
 ) {
 
     suspend fun storeAppToken(accessToken: String, refreshToken: String) {
-        dataStore.edit{
+        dataStore.edit {
             it[ACCESS_TOKEN] = accessToken
             it[REFRESH_TOKEN] = refreshToken
         }
     }
 
-    fun getAppToken(): Flow<String?> =
-        dataStore.data.map { it[ACCESS_TOKEN] }
+    fun getAppToken(): Flow<String?> = dataStore.data.map { it[ACCESS_TOKEN] }
 
+    suspend fun fetchUserProfile(userProfile: UserProfile) {
+        dataStore.edit {
+            it[USER_PROFILE_IMAGE] = userProfile.profileImage.orEmpty()
+            it[USER_NICKNAME] = userProfile.nickname
+        }
+    }
+
+    fun getUserProfile(): Flow<UserProfile> {
+        return dataStore.data
+            .map {
+                it[USER_PROFILE_IMAGE]
+            }
+            .combine(dataStore.data.map { it[USER_NICKNAME] }) { userProfileImage, userNickName ->
+                userNickName ?: throw NoDataException()
+                UserProfile(userProfileImage, userNickName)
+            }
+    }
 
 
     companion object {
         val ACCESS_TOKEN = stringPreferencesKey("accessToken")
         val REFRESH_TOKEN = stringPreferencesKey("refreshToken")
+        val USER_PROFILE_IMAGE = stringPreferencesKey("userProfileImage")
+        val USER_NICKNAME = stringPreferencesKey("userNickName")
     }
 }
--- android/app/src/main/java/com/teameetmeet/meetmeet/data/repository/UserRepository.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/data/repository/UserRepository.kt
@@ -2,11 +2,13 @@ package com.teameetmeet.meetmeet.data.repository
 
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import com.teameetmeet.meetmeet.data.NoDataException
 import com.teameetmeet.meetmeet.data.local.datastore.DataStoreHelper
 import com.teameetmeet.meetmeet.data.network.api.UserApi
 import com.teameetmeet.meetmeet.data.network.entity.UserProfile
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
@@ -16,14 +18,26 @@ class UserRepository @Inject constructor(
     private val userApi: UserApi,
     private val dataStore: DataStoreHelper
 ) {
-    fun getUserProfile(accessToken: String): Flow<UserProfile> {
+    fun getUserProfile(): Flow<UserProfile> {
         return flowOf(true)
             .map {
-                userApi.getUserProfile(accessToken)
+                val token = dataStore.getAppToken().first() ?: throw NoDataException()
+                userApi.getUserProfile(token)
             }.catch {
                 throw it
                 //TODO(예외 처리 필요)
             }
+    }
+
+    fun getLocalUserProfile(): Flow<UserProfile> {
+        return dataStore.getUserProfile().catch {
+                throw it
+                //TODO(예외 처리 필요)
+            }
+    }
+
+    suspend fun fetchUserProfile(userProfile: UserProfile) {
+        dataStore.fetchUserProfile(userProfile)
     }
 
     fun login(email: String, password: String): Flow<Boolean> = flow {
