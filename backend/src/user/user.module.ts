7e9f81e95d6a48badc899d22cfd4168f9db51ead
cdj2073 <daioww60@gmail.com>
2023-11-19 09:28:33 +0900

    feat: User Module 회원가입 기능 구현 (#50)

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -3,8 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Content } from 'src/content/entities/content.entity';
 import { OauthProvider } from './entities/oauthProvider.entity';
 import { User } from './entities/user.entity';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, OauthProvider, Content])],
+  providers: [UserService],
+  controllers: [UserController],
+  exports: [UserService],
 })
 export class UserModule {}
