e28ca8bb352e217216d167be88fce694b3bdfdac
agfalcon <agfalcon12@gmail.com>
2023-11-20 23:36:43 +0900

    feat: 이벤트 스토리 화면 tob app bar 버튼 클릭 기능 구현 (#16)

---
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/eventstory/EventStoryDetailFragment.kt
@@ -0,0 +1,9 @@
+package com.teameetmeet.meetmeet.presentation.eventstory
+
+import com.teameetmeet.meetmeet.R
+import com.teameetmeet.meetmeet.databinding.FragmentEventStoryBinding
+import com.teameetmeet.meetmeet.presentation.base.BaseFragment
+
+class EventStoryDetailFragment : BaseFragment<FragmentEventStoryBinding>(R.layout.fragment_event_story_detail) {
+
+}
