2c038a086f455c537e1d39d207e251b785169a1f
agfalcon <agfalcon12@gmail.com>
2023-11-20 20:49:52 +0900

    feat: ㄷEventActivity 생성 및 데이터 바인딩, nav_graph 설정 (#16)

---
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/eventstory/EventStoryActivity.kt
@@ -0,0 +1,11 @@
+package com.teameetmeet.meetmeet.presentation.eventstory
+
+import com.teameetmeet.meetmeet.R
+import com.teameetmeet.meetmeet.databinding.ActivityEventStoryBinding
+import com.teameetmeet.meetmeet.presentation.base.BaseActivity
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EventStoryActivity : BaseActivity<ActivityEventStoryBinding>(R.layout.activity_event_story) {
+
+}
