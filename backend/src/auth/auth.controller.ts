d165bb7f072d5990000a9f68b906ac0984824542
cdj2073 <daioww60@gmail.com>
2023-11-20 22:16:31 +0900

    docs: swagger 적용 (#47)

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { User } from 'src/user/entities/user.entity';
 import { AuthService } from './auth.service';
 import { AuthUserDto } from './dto/auth-user.dto';
 import { GetUser } from './get-user.decorator';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -14,6 +16,10 @@ export class AuthController {
     return this.authService.register(authUserDto);
   }
 
+  @ApiBody({
+    description: 'post swagger',
+    type: AuthUserDto,
+  })
   @UseGuards(AuthGuard('local'))
   @Post('login')
   login(@GetUser() user: User) {
