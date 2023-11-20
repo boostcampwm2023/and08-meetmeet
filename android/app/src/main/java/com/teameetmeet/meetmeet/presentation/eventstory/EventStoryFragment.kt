ad1839a38e80f85ca97c556cc204eb043d7361a2
agfalcon <agfalcon12@gmail.com>
2023-11-20 20:52:51 +0900

    feat: EventStoryFragment 생성 및 데이터 바인딩, nav_graph에 등록 (#16)

---
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/eventstory/EventStoryFragment.kt
@@ -0,0 +1,9 @@
+package com.teameetmeet.meetmeet.presentation.eventstory
+
+import com.teameetmeet.meetmeet.R
+import com.teameetmeet.meetmeet.databinding.FragmentEventStoryBinding
+import com.teameetmeet.meetmeet.presentation.base.BaseFragment
+
+class EventStoryFragment : BaseFragment<FragmentEventStoryBinding>(R.layout.fragment_event_story) {
+
+}
