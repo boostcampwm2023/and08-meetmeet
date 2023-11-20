4464c38735373dd7650b8f776a356af606e1b45a
LeeHaiLim <hailim1997@gmail.com>
2023-11-20 19:45:41 +0900

    feat: setting repository 추가(#54)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/setting/alarm/SettingAlarmViewModel.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/setting/alarm/SettingAlarmViewModel.kt
@@ -2,7 +2,7 @@ package com.teameetmeet.meetmeet.presentation.setting.alarm
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.teameetmeet.meetmeet.data.local.datastore.DataStoreHelper
+import com.teameetmeet.meetmeet.data.repository.SettingRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,7 +13,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SettingAlarmViewModel @Inject constructor(
-    private val dataStoreHelper: DataStoreHelper
+    private val settingRepository: SettingRepository
 ) : ViewModel() {
     private val _isPushAlarmOn = MutableStateFlow<Boolean>(false)
     val isPushAlarmOn: StateFlow<Boolean> = _isPushAlarmOn
@@ -24,7 +24,7 @@ class SettingAlarmViewModel @Inject constructor(
 
     private fun getStoredAlarmState() {
         viewModelScope.launch(Dispatchers.IO) {
-            dataStoreHelper
+            settingRepository
                 .getAlarmState()
                 .collectLatest {
                     _isPushAlarmOn.emit(it)
@@ -34,7 +34,7 @@ class SettingAlarmViewModel @Inject constructor(
 
     fun changeAlarmState(isOn: Boolean) {
         viewModelScope.launch {
-            dataStoreHelper.storeAlarmState(isOn)
+            settingRepository.storeAlarmState(isOn)
         }
     }
 }
