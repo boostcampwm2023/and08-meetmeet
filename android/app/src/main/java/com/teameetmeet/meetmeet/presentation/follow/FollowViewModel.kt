85e8f570f2b12c605ffe8657e31e7603532167de
p-chanmin <pc_m111@naver.com>
2023-11-21 01:43:55 +0900

    feat: follow 화면 UI 연결 (#22)

---
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/follow/FollowViewModel.kt
@@ -0,0 +1,38 @@
+package com.teameetmeet.meetmeet.presentation.follow
+
+import androidx.lifecycle.ViewModel
+import com.teameetmeet.meetmeet.data.network.entity.UserProfile
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class FollowViewModel @Inject constructor() : ViewModel() {
+
+    private val _users: MutableStateFlow<List<UserProfile>> = MutableStateFlow(listOf())
+    val users: StateFlow<List<UserProfile>> = _users
+
+    fun getFollower() {
+        val tmp = listOf(
+            UserProfile(null, "팔로워1"),
+            UserProfile(null, "팔로워2"),
+            UserProfile(null, "팔로워3"),
+            UserProfile(null, "팔로워4"),
+            UserProfile(null, "팔로워5"),
+        )
+        _users.update { tmp }
+    }
+
+    fun getFollowing() {
+        val tmp = listOf(
+            UserProfile(null, "팔로잉1"),
+            UserProfile(null, "팔로잉2"),
+            UserProfile(null, "팔로잉3"),
+            UserProfile(null, "팔로잉4"),
+            UserProfile(null, "팔로잉5"),
+        )
+        _users.update { tmp }
+    }
+}
