b4ca48fe077b27f399f7b44452a6b13728b546c7
LeeHaiLim <hailim1997@gmail.com>
2023-11-22 02:27:17 +0900

    feat: 미디어 취소 버튼 클릭 리스너 인터페이스 (#29)

---
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/story/createfeed/MediaItemCancelClickListener.kt
@@ -0,0 +1,7 @@
+package com.teameetmeet.meetmeet.presentation.story.createfeed
+
+import com.teameetmeet.meetmeet.presentation.model.MediaItem
+
+interface MediaItemCancelClickListener {
+    fun onClick(mediaItem: MediaItem)
+}
