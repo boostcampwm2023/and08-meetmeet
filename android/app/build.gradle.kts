80c2495b1ad4a6ff0f3cede658750c9464d8b9d2
agfalcon <agfalcon12@gmail.com>
2023-11-21 19:48:32 +0900

    build: paging3 dependency 추가  (#16)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -101,4 +101,7 @@ dependencies {
     //Glide(이미지 로더)
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
+    //paging3
+    implementation("androidx.paging:paging-runtime:$3.1.1")
 }
