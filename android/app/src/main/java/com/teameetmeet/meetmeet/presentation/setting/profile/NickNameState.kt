6d77ada1ca01ac246d62c08c61400d5124d16a4e
p-chanmin <pc_m111@naver.com>
2023-11-22 01:12:09 +0900

    feat: NicknameState 상태 추가 (#55)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/setting/profile/NickNameState.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/setting/profile/NickNameState.kt
@@ -2,6 +2,7 @@ package com.teameetmeet.meetmeet.presentation.setting.profile
 
 sealed class NickNameState {
     data object None : NickNameState()
+    data object Same : NickNameState()
     data object Invalid : NickNameState()
     data object Valid : NickNameState()
 }
