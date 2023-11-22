ffdc554e1ce10dc2ce45801ca3c4abfe7dd84386
LeeHaiLim <hailim1997@gmail.com>
2023-11-22 16:56:05 +0900

    chore: 패키지 이동

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/eventstory/createfeed/CreateFeedBindingAdapter.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/eventstory/createfeed/CreateFeedBindingAdapter.kt
@@ -1,4 +1,4 @@
-package com.teameetmeet.meetmeet.presentation.story.createfeed
+package com.teameetmeet.meetmeet.presentation.eventstory.createfeed
 
 import android.net.Uri
 import android.widget.ImageView
