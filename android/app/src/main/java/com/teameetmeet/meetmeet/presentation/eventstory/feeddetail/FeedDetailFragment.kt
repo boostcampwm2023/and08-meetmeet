f47126c3f9d10f130af2397a5af0dd4bfb59f8ea
p-chanmin <pc_m111@naver.com>
2023-11-22 21:48:54 +0900

    chore: feed detail 패키지 이동 (#30)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/eventstory/feeddetail/FeedDetailFragment.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/eventstory/feeddetail/FeedDetailFragment.kt
@@ -1,4 +1,4 @@
-package com.teameetmeet.meetmeet.presentation.story.feeddetail
+package com.teameetmeet.meetmeet.presentation.eventstory.feeddetail
 
 import android.os.Bundle
 import android.view.View
