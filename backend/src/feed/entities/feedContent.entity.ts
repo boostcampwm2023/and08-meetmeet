1391c3d901e7d159094692a10e45d681d4aa26e7
cdj2073 <daioww60@gmail.com>
2023-11-23 02:21:50 +0900

    feat: FeedContent 엔티티 작성 (#29)

--- backend/src/feed/entities/feedContent.entity.ts
+++ backend/src/feed/entities/feedContent.entity.ts
@@ -1,1 +1,18 @@
-export class FeedContent {}
+import { Content } from 'src/content/entities/content.entity';
+import { Entity, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Feed } from './feed.entity';
+
+@Entity()
+export class FeedContent {
+  @PrimaryColumn()
+  feedId: number;
+
+  @PrimaryColumn()
+  contentId: number;
+
+  @ManyToOne(() => Feed, { nullable: false })
+  feed: Feed;
+
+  @ManyToOne(() => Content, { nullable: false })
+  content: Content;
+}
