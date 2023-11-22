41b1963d3398c9042abf2717d09c62a7bbf20a4e
LeeHaiLim <hailim1997@gmail.com>
2023-11-22 16:11:14 +0900

    refactor: 바인딩 아답터 추상화(#15)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/calendar/CalendarBindingAdapter.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/calendar/CalendarBindingAdapter.kt
@@ -2,23 +2,10 @@ package com.teameetmeet.meetmeet.presentation.calendar
 
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.RecyclerView
-import com.teameetmeet.meetmeet.presentation.model.CalendarItem
 import com.teameetmeet.meetmeet.util.toYearMonth
 import java.time.LocalDate
 
 @BindingAdapter("local_date")
-fun TextView.bindLocalDate( localDate: LocalDate) {
+fun TextView.bindLocalDate(localDate: LocalDate) {
     text = localDate.toYearMonth()
-}
-
-@BindingAdapter("day_list", "vm")
-fun RecyclerView.bindDayInMonth(
-    dayList: List<CalendarItem>,
-    viewModel: CalendarViewModel
-) {
-    if (adapter == null) {
-        adapter = CalendarAdapter(viewModel)
-    }
-    (adapter as CalendarAdapter).submitList(dayList)
 }
