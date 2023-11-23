be538cef85a8180ede27a2d099675a40d303f463
agfalcon <agfalcon12@gmail.com>
2023-11-23 12:08:21 +0900

    refactor: Long Extension 불필요한 함수 삭제

--- android/app/src/main/java/com/teameetmeet/meetmeet/util/LongExtension.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/util/LongExtension.kt
@@ -1,14 +1,9 @@
 package com.teameetmeet.meetmeet.util
 
-import com.teameetmeet.meetmeet.MeetMeetApp
-import com.teameetmeet.meetmeet.R
-import java.text.SimpleDateFormat
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
-import java.util.Date
-import java.util.Locale
 
 fun Long.toLocalDate(zoneId: ZoneId = ZoneId.systemDefault()): LocalDate {
     return Instant.ofEpochMilli(this).atZone(zoneId).toLocalDate()
@@ -32,22 +27,4 @@ fun Long.toDateString(format: DateTimeFormat, zoneId: ZoneId = ZoneId.systemDefa
 
 fun String.toTimeStampLong(format: DateTimeFormat, zoneId: ZoneId = ZoneId.systemDefault()): Long {
     return LocalDateTime.parse(this, format.formatter).toLong(zoneId)
-}
-
-fun Long.toDateStringFormat(locale: Locale = Locale.KOREA): String {
-    val dateFormat =
-        SimpleDateFormat(MeetMeetApp.instance.getString(R.string.common_korea_date_format), locale)
-    val date = Date(this)
-    return dateFormat.format(date)
-}
-
-fun String.toTimeStampLong(locale: Locale = Locale.KOREA): Long {
-    val dateFormat =
-        SimpleDateFormat(MeetMeetApp.instance.getString(R.string.common_korea_date_format), locale)
-    return try {
-        val date = dateFormat.parse(this)
-        date.time
-    } catch (e: Exception) {
-        0
-    }
 }
