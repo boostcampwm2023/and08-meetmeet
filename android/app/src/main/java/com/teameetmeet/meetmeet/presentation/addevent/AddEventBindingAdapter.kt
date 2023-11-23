3355909e0e46e55b6c56948b279622d1fa2b3fbe
p-chanmin <pc_m111@naver.com>
2023-11-24 03:15:37 +0900

    feat: 일정 추가 viewModel databinding 연결 (#28)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/addevent/AddEventBindingAdapter.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/addevent/AddEventBindingAdapter.kt
@@ -2,11 +2,10 @@ package com.teameetmeet.meetmeet.presentation.addevent
 
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
-@BindingAdapter("long_date_text")
-fun TextView.bindLongDateText(date: Long) {
-    this.text = SimpleDateFormat("yyyy-MM-dd", Locale.KOREA).format(Date(date))
+@BindingAdapter("date_text")
+fun TextView.bindLongDateText(date: LocalDateTime) {
+    this.text = date.format(DateTimeFormatter.ISO_DATE)
 }
