c5f04b4d7779114e98e86e2e93453f0bb0e4d919
cdj2073 <daioww60@gmail.com>
2023-11-24 20:08:08 +0900

    feat: JwtAuthGuard 수정 (#134)
    유효하지 않은 토큰이나 토큰 만료에 대한 에러는 418 상태코드로
    다른 401 에러와 구분하기 위해 JwtAuthGuard 수정

--- backend/src/auth/jwt-auth.guard.ts
+++ backend/src/auth/jwt-auth.guard.ts
@@ -1,5 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { ImATeapotException, Injectable } from '@nestjs/common';
+import { TokenExpiredError } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest<User>(err: Error, user: User): User {
+    if (err instanceof TokenExpiredError) {
+      throw new ImATeapotException('Access token is expired.');
+    }
+    if (!user) {
+      throw new ImATeapotException('Invalid token.');
+    }
+    if (err instanceof Error) {
+      throw new ImATeapotException(err.message);
+    }
+    return user;
+  }
+}
