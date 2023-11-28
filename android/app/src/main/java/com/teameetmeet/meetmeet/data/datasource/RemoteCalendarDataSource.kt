277793cbc46fe9e660b435f871f4547a15ffd32a
LeeHaiLim <hailim1997@gmail.com>
2023-11-28 23:43:17 +0900

    enhancement: api 변경 반영 (#34)

--- android/app/src/main/java/com/teameetmeet/meetmeet/data/datasource/RemoteCalendarDataSource.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/data/datasource/RemoteCalendarDataSource.kt
@@ -22,12 +22,12 @@ class RemoteCalendarDataSource @Inject constructor(private val api: CalendarApi)
     }
 
     fun searchEvents(
-        keyword: String? = null,
+        keyword: String?,
         startDate: String,
         endDate: String
     ): Flow<List<EventResponse>> {
         return flowOf(true)
-            .map { api.searchEvents(keyword, startDate, endDate) }
+            .map { api.searchEvents(keyword, startDate, endDate).events }
             .catch {
                 //todo: 예외 처리
             }
