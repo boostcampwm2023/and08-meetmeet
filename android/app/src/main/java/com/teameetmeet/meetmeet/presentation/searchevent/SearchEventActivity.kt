4a77764561133a0d55253cdb9d2ca3525d8f2f34
LeeHaiLim <hailim1997@gmail.com>
2023-11-28 23:40:15 +0900

    feat: search event 프래그먼트로 코드 옮김(#34)
    - jetpack navigation 사용 위함

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/searchevent/SearchEventActivity.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/searchevent/SearchEventActivity.kt
@@ -1,99 +1,11 @@
 package com.teameetmeet.meetmeet.presentation.searchevent
 
-import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.viewModels
-import androidx.core.util.Pair
-import androidx.lifecycle.lifecycleScope
-import com.google.android.material.datepicker.CalendarConstraints
-import com.google.android.material.datepicker.MaterialDatePicker
 import com.teameetmeet.meetmeet.R
 import com.teameetmeet.meetmeet.databinding.ActivitySearchEventBinding
 import com.teameetmeet.meetmeet.presentation.base.BaseActivity
-import com.teameetmeet.meetmeet.util.addUtcTimeOffset
-import com.teameetmeet.meetmeet.util.toLocalDateTime
-import com.teameetmeet.meetmeet.util.toLong
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
-import java.time.ZoneId
 
 @AndroidEntryPoint
-class SearchActivity : BaseActivity<ActivitySearchEventBinding>(
+class SearchEventActivity : BaseActivity<ActivitySearchEventBinding>(
     R.layout.activity_search_event
-) {
-    private val viewModel: SearchEventViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        with(binding) {
-            vm = viewModel
-            searchEventSv.setupWithSearchBar(searchEventSb)
-            searchEventSpinner.setSelection(1)
-            searchEventSb.setNavigationOnClickListener {
-                finish()
-            }
-        }
-        setDatePicker()
-    }
-
-    private fun setDatePicker() {
-        val datePickerBuilder =
-            MaterialDatePicker.Builder.dateRangePicker()
-                .setTitleText(getString(R.string.search_event_title))
-
-        val constraintsBuilder = CalendarConstraints.Builder()
-
-        showDatePicker(datePickerBuilder, constraintsBuilder)
-        setDatePickerBuilders(datePickerBuilder, constraintsBuilder)
-    }
-
-    private fun setDatePickerBuilders(
-        datePickerBuilder: MaterialDatePicker.Builder<Pair<Long, Long>>,
-        constraintsBuilder: CalendarConstraints.Builder
-    ) {
-        lifecycleScope.launch {
-            viewModel.searchDateRange.collectLatest {
-                datePickerBuilder.setSelection(
-                    Pair(it.first.addUtcTimeOffset(), it.second.addUtcTimeOffset())
-                )
-                constraintsBuilder.setOpenAt(it.first.addUtcTimeOffset())
-            }
-        }
-    }
-
-    private fun showDatePicker(
-        datePickerBuilder: MaterialDatePicker.Builder<Pair<Long, Long>>,
-        constraintsBuilder: CalendarConstraints.Builder
-    ) {
-        binding.searchEventTvDateRange.setOnClickListener {
-            datePickerBuilder
-                .setCalendarConstraints(constraintsBuilder.build())
-                .build()
-                .apply {
-                    this.addOnPositiveButtonClickListener {
-                        viewModel.setSearchDateRange(validateDateRange(it))
-                        binding.searchEventSpinner.setSelection(0)
-                    }
-                }.show(supportFragmentManager, "datePicker")
-        }
-    }
-
-    private fun validateDateRange(range: Pair<Long, Long>): Pair<Long, Long> {
-        val start = range.first.toLocalDateTime(ZoneId.of("UTC"))
-        val end = range.second.toLocalDateTime(ZoneId.of("UTC"))
-        val std = start.plusMonths(6)
-
-        return if (end.isAfter(std)) {
-            Toast.makeText(
-                this,
-                getString(R.string.search_event_range_constraint_message),
-                Toast.LENGTH_SHORT
-            ).show()
-            Pair(start.toLong(), std.toLong())
-        } else {
-            Pair(start.toLong(), end.toLong())
-        }
-    }
-}
+)
---
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/searchevent/searchevent/SearchEventFragment.kt
@@ -0,0 +1,144 @@
+package com.teameetmeet.meetmeet.presentation.searchevent.searchevent
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.util.Pair
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.teameetmeet.meetmeet.R
+import com.teameetmeet.meetmeet.data.network.entity.EventResponse
+import com.teameetmeet.meetmeet.databinding.FragmentSearchEventBinding
+import com.teameetmeet.meetmeet.presentation.base.BaseFragment
+import com.teameetmeet.meetmeet.presentation.eventstory.eventstory.OnItemClickListener
+import com.teameetmeet.meetmeet.util.addUtcTimeOffset
+import com.teameetmeet.meetmeet.util.toLocalDate
+import com.teameetmeet.meetmeet.util.toStartLong
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import java.time.ZoneId
+
+@AndroidEntryPoint
+class SearchEventFragment : BaseFragment<FragmentSearchEventBinding>(
+    R.layout.fragment_search_event
+), OnItemClickListener, SearchItemClickListener {
+    private val viewModel: SearchEventViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setBinding()
+        setSearch()
+        setDatePicker()
+    }
+
+    private fun setBinding() {
+        with(binding) {
+            vm = viewModel
+            searchEventSpinner.setSelection(1)
+            searchEventRv.adapter =
+                SearchResultEventAdapter(this@SearchEventFragment, this@SearchEventFragment)
+            searchEventIbNavPre.setOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
+    }
+
+    private fun setSearch() {
+        with(binding) {
+            searchEventEtSearch.setOnEditorActionListener { v, actionId, event ->
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    performSearch()
+                    true
+                }
+                false
+            }
+            searchEventIbSearch.setOnClickListener {
+                performSearch()
+                searchEventEtSearch.clearFocus()
+            }
+        }
+    }
+
+    private fun performSearch() {
+        val imm =
+            requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.searchEventEtSearch.windowToken, 0)
+        viewModel.searchEvents()
+    }
+
+    private fun setDatePicker() {
+        val datePickerBuilder =
+            MaterialDatePicker.Builder.dateRangePicker()
+                .setTitleText(getString(R.string.search_event_title))
+
+        val constraintsBuilder = CalendarConstraints.Builder()
+
+        showDatePicker(datePickerBuilder, constraintsBuilder)
+        setDatePickerBuilders(datePickerBuilder, constraintsBuilder)
+    }
+
+    private fun setDatePickerBuilders(
+        datePickerBuilder: MaterialDatePicker.Builder<Pair<Long, Long>>,
+        constraintsBuilder: CalendarConstraints.Builder
+    ) {
+        lifecycleScope.launch {
+            viewModel.searchDateRange.collectLatest {
+                datePickerBuilder.setSelection(
+                    Pair(it.first.addUtcTimeOffset(), it.second.addUtcTimeOffset())
+                )
+                constraintsBuilder.setOpenAt(it.first.addUtcTimeOffset())
+            }
+        }
+    }
+
+    private fun showDatePicker(
+        datePickerBuilder: MaterialDatePicker.Builder<Pair<Long, Long>>,
+        constraintsBuilder: CalendarConstraints.Builder
+    ) {
+        binding.searchEventTvDateRange.setOnClickListener {
+            datePickerBuilder
+                .setCalendarConstraints(constraintsBuilder.build())
+                .build()
+                .apply {
+                    this.addOnPositiveButtonClickListener {
+                        viewModel.setSearchDateRange(validateDateRange(it))
+                        binding.searchEventSpinner.setSelection(0)
+                    }
+                }.show(requireActivity().supportFragmentManager, "datePicker")
+        }
+    }
+
+    private fun validateDateRange(range: Pair<Long, Long>): Pair<Long, Long> {
+        val start = range.first.toLocalDate(ZoneId.of("UTC"))
+        val end = range.second.toLocalDate(ZoneId.of("UTC"))
+        val std = start.plusDays(179)
+
+        return if (end.isAfter(std)) {
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.search_event_range_constraint_message),
+                Toast.LENGTH_SHORT
+            ).show()
+            Pair(start.toStartLong(), std.toStartLong())
+        } else {
+            Pair(start.toStartLong(), end.toStartLong())
+        }
+    }
+
+    override fun onItemClick() {
+        // todo: 프로필 이미지로..
+    }
+
+    override fun onClick(event: EventResponse) {
+        findNavController().navigate(
+            SearchEventFragmentDirections.actionSearchEventFragmentToEventStoryActivity()
+        )
+    }
+}
