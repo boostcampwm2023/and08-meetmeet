b9de2bcafa58ed38b5834ad224c8a08309b3f30a
p-chanmin <pc_m111@naver.com>
2023-11-29 02:56:38 +0900

    feat: 비밀번호 변경 API viewModel 로직 추가 (#56)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/setting/passwordchange/SettingPasswordChangeFragment.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/setting/passwordchange/SettingPasswordChangeFragment.kt
@@ -2,25 +2,54 @@ package com.teameetmeet.meetmeet.presentation.setting.passwordchange
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.teameetmeet.meetmeet.R
 import com.teameetmeet.meetmeet.databinding.FragmentSettingPasswordChangeBinding
 import com.teameetmeet.meetmeet.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SettingPasswordChangeFragment :
     BaseFragment<FragmentSettingPasswordChangeBinding>(R.layout.fragment_setting_password_change) {
 
+    private val viewModel: SettingPasswordChangeViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.vm = viewModel
+
         setTopAppBar()
+        collectViewModelEvent()
     }
 
     private fun setTopAppBar() {
         binding.topAppBar.setNavigationOnClickListener {
             findNavController().popBackStack()
+        }
+    }
+
+    private fun collectViewModelEvent() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.event.collectLatest { event ->
+                    when (event) {
+                        is SettingPasswordChangeUiEvent.NavigateToSettingHomeFragment -> {
+                            findNavController().popBackStack()
+                        }
+
+                        is SettingPasswordChangeUiEvent.ShowMessage -> {
+                            showMessage(event.message, event.extraMessage)
+                        }
+                    }
+                }
+            }
         }
     }
 }
---
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/setting/passwordchange/SettingPasswordChangeViewModel.kt
@@ -0,0 +1,94 @@
+package com.teameetmeet.meetmeet.presentation.setting.passwordchange
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.teameetmeet.meetmeet.R
+import com.teameetmeet.meetmeet.data.repository.UserRepository
+import com.teameetmeet.meetmeet.presentation.login.signup.PasswordState
+import com.teameetmeet.meetmeet.util.Verification
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingPasswordChangeViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+    private val _uiState: MutableStateFlow<SettingPasswordChangeUiState> =
+        MutableStateFlow(SettingPasswordChangeUiState())
+    val uiState: StateFlow<SettingPasswordChangeUiState> = _uiState
+
+    private val _event: MutableSharedFlow<SettingPasswordChangeUiEvent> = MutableSharedFlow()
+    val event: SharedFlow<SettingPasswordChangeUiEvent> = _event
+
+    private val _showPlaceholder: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val showPlaceholder: StateFlow<Boolean> = _showPlaceholder
+
+    fun updatePassword(password: CharSequence?) {
+        val passwordString = password.toString()
+        val passwordState = getPasswordStateOf(passwordString)
+        val passwordConfirmState =
+            getPasswordConfirmStateOf(passwordString, _uiState.value.passwordConfirm)
+        _uiState.update {
+            it.copy(
+                password = passwordString,
+                passwordState = passwordState,
+                passwordConfirmState = passwordConfirmState,
+                submitEnable = passwordState == PasswordState.Valid && passwordConfirmState == PasswordState.Valid
+            )
+        }
+    }
+
+    fun updatePasswordConfirm(passwordConfirm: CharSequence?) {
+        val passwordConfirmString = passwordConfirm.toString()
+        val state = getPasswordConfirmStateOf(_uiState.value.password, passwordConfirmString)
+        _uiState.update {
+            it.copy(
+                passwordConfirm = passwordConfirmString,
+                passwordConfirmState = state,
+                submitEnable = _uiState.value.passwordState == PasswordState.Valid && state == PasswordState.Valid
+            )
+        }
+    }
+
+    fun patchPassword() {
+        viewModelScope.launch {
+            _showPlaceholder.update { true }
+            userRepository.patchPassword(_uiState.value.password)
+                .catch {
+                    _event.emit(SettingPasswordChangeUiEvent.ShowMessage(R.string.setting_password_change_fail))
+                    _showPlaceholder.update { false }
+                }.collectLatest {
+                    _event.emit(SettingPasswordChangeUiEvent.NavigateToSettingHomeFragment)
+                    _showPlaceholder.update { false }
+                }
+        }
+    }
+
+    private fun getPasswordStateOf(password: String): PasswordState {
+        return when {
+            password.isEmpty() -> PasswordState.None
+            Verification.isValidPassword(password) -> PasswordState.Valid
+            else -> PasswordState.Invalid
+        }
+    }
+
+    private fun getPasswordConfirmStateOf(
+        password: String,
+        passwordConfirm: String
+    ): PasswordState {
+        return when {
+            passwordConfirm.isEmpty() -> PasswordState.None
+            password == passwordConfirm -> PasswordState.Valid
+            else -> PasswordState.Invalid
+        }
+    }
+}
