3de7fa972c2d0dd19e2f9186bd1de64df9d38e38
cdj2073 <daioww60@gmail.com>
2023-11-30 16:13:04 +0900

    feat: file path에 prefix 추가

--- backend/src/content/content.service.ts
+++ backend/src/content/content.service.ts
@@ -1,20 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { v4 as uuidv4 } from 'uuid';
 import { In, Repository } from 'typeorm';
 import { Content } from './entities/content.entity';
 import { ObjectStorage } from './object-storage';
 import { extname } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ContentService {
+  locationPrefix: string;
   constructor(
     @InjectRepository(Content) private contentRepository: Repository<Content>,
     private readonly objectStorage: ObjectStorage,
-  ) {}
+    private readonly configService: ConfigService,
+  ) {
+    this.locationPrefix = configService.get(
+      'CONTENT_PREFIX',
+      'https://kr.object.ncloudstorage.com/meetmeet/',
+    );
+  }
 
   async createContent(file: Express.Multer.File, dir: string) {
-    file.path = this.generateFilePath(dir, file.originalname);
+    file.path =
+      this.locationPrefix + this.generateFilePath(dir, file.originalname);
     const content = this.createEntity(file);
 
     await this.objectStorage.upload(file);
@@ -23,7 +32,8 @@ export class ContentService {
 
   async createContentBulk(files: Array<Express.Multer.File>, dir: string) {
     const contents = files.reduce((acc, file) => {
-      file.path = this.generateFilePath(dir, file.originalname);
+      file.path =
+        this.locationPrefix + this.generateFilePath(dir, file.originalname);
       const content = this.createEntity(file);
       return [...acc, content];
     }, []);
@@ -75,7 +85,14 @@ export class ContentService {
 
     await this.contentRepository.softDelete(idList);
     // TODO: delete 시 object storage에서 바로 삭제할 것인지
-    await this.objectStorage.deleteBulk(files.map((file) => file.path));
+    await this.objectStorage.deleteBulk(
+      files.map((file) => {
+        if (!file) {
+          throw new NotFoundException('File Not Found');
+        }
+        return file.path.replace(this.locationPrefix, '');
+      }),
+    );
   }
 
   generateFilePath(dir: string, fileName: string) {
