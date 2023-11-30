844704c70e18588ec0258e4882986aa2488f0b34
agfalcon <agfalcon12@gmail.com>
2023-12-01 02:48:49 +0900

    feat: event notification 목업 띄우기 기능 구현 (#23)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/notification/event/EventNotificationFragment.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/notification/event/EventNotificationFragment.kt
@@ -1,8 +1,42 @@
 package com.teameetmeet.meetmeet.presentation.notification.event
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.teameetmeet.meetmeet.R
 import com.teameetmeet.meetmeet.databinding.FragmentNotificationBinding
 import com.teameetmeet.meetmeet.presentation.base.BaseFragment
+import kotlinx.coroutines.launch
 
 class EventNotificationFragment : BaseFragment<FragmentNotificationBinding>(R.layout.fragment_notification) {
+
+    private val viewModel: EventNotificationViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setBinding()
+        collectViewModelData()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.fetchEventNotificationList()
+    }
+
+    private fun collectViewModelData() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.eventNotificationList.collect {
+                    (binding.notificationRcv.adapter as? EventNotificationAdapter)?.submitList(it)
+                }
+            }
+        }
+    }
+
+    private fun setBinding() {
+        binding.notificationRcv.adapter = EventNotificationAdapter()
+    }
 }
