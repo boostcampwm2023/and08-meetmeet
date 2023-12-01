10e0363fb14f6b688d0d8364e23dafec230c1b97
p-chanmin <pc_m111@naver.com>
2023-12-02 01:39:23 +0900

    feat: 프로필 이미지 변경, 닉네임 변경 API 적용 (#55)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/setting/profile/SettingProfileUiState.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/setting/profile/SettingProfileUiState.kt
@@ -5,8 +5,8 @@ import com.teameetmeet.meetmeet.data.model.UserProfile
 
 data class SettingProfileUiState(
     val currentUserProfile: UserProfile = UserProfile(null, "", ""),
-    val profileImage: Uri? = null,
+    val profileImage: Uri? = Uri.parse(""),
     val nickname: String = "",
     val duplicatedEnable: Boolean = false,
-    val nickNameState: NickNameState = NickNameState.Same,
+    val nickNameState: NickNameState = NickNameState.None,
 )
