3defebcb5208e5e0fe877d259755d1c3879034b1
LeeHaiLim <hailim1997@gmail.com>
2023-12-04 22:51:07 +0900

    feat: home activity 진입 시 권한 확인하는 기능 (#17) - 알림 권한 확인 후 필요시 권한 요청
    Co-authored-by: p-chanmin <pc_m111@naver.com>
    Co-authored-by: agfalcon <agfalcon12@gmail.com>
    Co-authored-by: LeeHaiLim <hailim1997@gmail.com>

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/home/HomeActivity.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/home/HomeActivity.kt
@@ -1,6 +1,12 @@
 package com.teameetmeet.meetmeet.presentation.home
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -14,12 +20,42 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
 
     private lateinit var navController: NavController
 
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (!isGranted) {
+            showMessage(R.string.notification_message_permission_denied)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        askNotificationPermission()
         val navHost =
             supportFragmentManager.findFragmentById(binding.fragmentContainer.id) as NavHostFragment
         navController = navHost.navController
         binding.bottomNav.setupWithNavController(navController)
+    }
+
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.notification_title_request_permission))
+                    .setMessage(getString(R.string.notification_message_request_permission))
+                    .setPositiveButton(getString(R.string.notification_positive_request_permission)) { _, _ ->
+                        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                    }
+                    .setNegativeButton(getString(R.string.notification_negative_request_permission)) { _, _ ->
+                    }
+                    .show()
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
     }
 }
