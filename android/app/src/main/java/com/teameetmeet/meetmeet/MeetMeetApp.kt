ad88b9606e6dec6134d0ba925904852474882897
LeeHaiLim <hailim1997@gmail.com>
2023-12-04 22:51:57 +0900

    refactor: 알림 채널 생성 위치 변경(#17)
    Co-authored-by: p-chanmin <pc_m111@naver.com>
    Co-authored-by: agfalcon <agfalcon12@gmail.com>
    Co-authored-by: LeeHaiLim <hailim1997@gmail.com>

--- android/app/src/main/java/com/teameetmeet/meetmeet/MeetMeetApp.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/MeetMeetApp.kt
@@ -1,11 +1,8 @@
 package com.teameetmeet.meetmeet
 
 import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.content.Context
 import com.kakao.sdk.common.KakaoSdk
-import com.teameetmeet.meetmeet.service.notification.NotificationHelper
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -14,27 +11,15 @@ class MeetMeetApp : Application() {
     init {
         instance = this
     }
+
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
-        createNotificationChannel()
-    }
-
-    private fun createNotificationChannel() {
-        val name = getString(R.string.notification_title_event_notification)
-        val descriptionText = getString(R.string.notification_channel_description_event)
-        val importance = NotificationManager.IMPORTANCE_HIGH
-        val channel = NotificationChannel(NotificationHelper.CHANNEL_ID_EVENT_NOTIFICATION, name, importance).apply {
-            description = descriptionText
-        }
-        val notificationManager: NotificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
     }
 
     companion object {
         lateinit var instance: MeetMeetApp
-        fun applicationContext() : Context {
+        fun applicationContext(): Context {
             return instance.applicationContext
         }
     }
