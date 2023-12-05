970f654b9c8d68e448f0b1f3018e86168e5d5af1
agfalcon <agfalcon12@gmail.com>
2023-12-05 12:20:56 +0900

    feat: 로그아웃/로그인 시 알람 설정 변경 (#26)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/home/HomeViewModel.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/home/HomeViewModel.kt
@@ -4,15 +4,26 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
+import com.teameetmeet.meetmeet.data.repository.CalendarRepository
 import com.teameetmeet.meetmeet.data.repository.UserRepository
+import com.teameetmeet.meetmeet.service.alarm.AlarmHelper
+import com.teameetmeet.meetmeet.util.date.getLocalDateTime
+import com.teameetmeet.meetmeet.util.date.toLong
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val calendarRepository: CalendarRepository,
+    private val alarmHelper: AlarmHelper
 ) : ViewModel() {
+
+    init {
+        setAlarm()
+    }
 
     fun updateFcmToken() {
         FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
@@ -24,5 +35,17 @@ class HomeViewModel @Inject constructor(
                 userRepository.updateFcmToken(token)
             }
         })
+    }
+
+    private fun setAlarm() {
+        viewModelScope.launch {
+            calendarRepository.getEvents(
+                getLocalDateTime().toLong(),
+                getLocalDateTime().plusDays(AlarmHelper.UPDATE_DAY_UNIT).toLong()
+            ).catch {}.collect {
+                alarmHelper.registerRepeatAlarm()
+            }
+        }
+
     }
 }
