95708147f10baee17cfcc2cd37aaade6d37e6b25
p-chanmin <pc_m111@naver.com>
2023-12-06 02:18:20 +0900

    refactor: 알림 목록 layout 분리 (#17)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/notification/follow/FollowNotificationFragment.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/notification/follow/FollowNotificationFragment.kt
@@ -5,13 +5,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.teameetmeet.meetmeet.R
-import com.teameetmeet.meetmeet.databinding.FragmentNotificationBinding
+import com.teameetmeet.meetmeet.databinding.FragmentFollowNotificationBinding
 import com.teameetmeet.meetmeet.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FollowNotificationFragment :
-    BaseFragment<FragmentNotificationBinding>(R.layout.fragment_notification) {
+    BaseFragment<FragmentFollowNotificationBinding>(R.layout.fragment_follow_notification) {
 
     private val viewModel: FollowNotificationViewModel by viewModels()
 
