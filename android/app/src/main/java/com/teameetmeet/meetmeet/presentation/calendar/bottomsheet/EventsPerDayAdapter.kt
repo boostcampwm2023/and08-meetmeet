ce142dbd26060dcce327a6015388ba5538779df1
LeeHaiLim <hailim1997@gmail.com>
2023-12-06 00:53:40 +0900

    feat: 이벤트 목록 날짜 형식에 날짜에 따라 다르게 표시(#36)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/calendar/bottomsheet/EventsPerDayAdapter.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/calendar/bottomsheet/EventsPerDayAdapter.kt
@@ -8,7 +8,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.teameetmeet.meetmeet.databinding.ItemEventSimpleBinding
 import com.teameetmeet.meetmeet.presentation.model.EventSimple
 import com.teameetmeet.meetmeet.util.date.DateTimeFormat
+import com.teameetmeet.meetmeet.util.date.getLocalDate
 import com.teameetmeet.meetmeet.util.date.toDateString
+import com.teameetmeet.meetmeet.util.date.toLocalDate
 
 class EventsPerDayAdapter(
     private val listener: EventItemClickListener
@@ -32,13 +34,37 @@ class EventsPerDayAdapter(
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: EventSimple) {
             binding.item = item
+            setDateText(item)
             itemView.setOnClickListener {
                 listener.onItemClick(item)
             }
+        }
+
+        private fun setDateText(item: EventSimple) {
+            val startDate = item.startDateTime.toLocalDate()
+            val endDate = item.endDateTime.toLocalDate()
+            val year = getLocalDate().year
+
             binding.itemEventBsTvDescription.text =
-                "${item.startDateTime.toDateString(DateTimeFormat.LOCAL_DATE_TIME)} ~ ${
-                    item.endDateTime.toDateString(DateTimeFormat.LOCAL_DATE_TIME)
-                }"
+                if (startDate == endDate) {
+                    "${
+                        item.startDateTime.toDateString(DateTimeFormat.LOCAL_TIME)
+                    } - ${
+                        item.endDateTime.toDateString(DateTimeFormat.LOCAL_TIME)
+                    }"
+                } else {
+                    "${
+                        item.startDateTime.toDateString(
+                            if (startDate.year == year) DateTimeFormat.LOCAL_DATE_TIME_WO_YEAR
+                            else DateTimeFormat.LOCAL_DATE_TIME
+                        )
+                    } - ${
+                        item.endDateTime.toDateString(
+                            if (endDate.year == year) DateTimeFormat.LOCAL_DATE_TIME_WO_YEAR
+                            else DateTimeFormat.LOCAL_DATE_TIME
+                        )
+                    }"
+                }
         }
     }
 
