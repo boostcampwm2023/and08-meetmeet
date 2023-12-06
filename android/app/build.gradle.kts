5ba38f518d17d587d51f8b3108d6e1fb44a02e98
p-chanmin <pc_m111@naver.com>
2023-12-07 03:25:15 +0900

    v0.1.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1.1"
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
