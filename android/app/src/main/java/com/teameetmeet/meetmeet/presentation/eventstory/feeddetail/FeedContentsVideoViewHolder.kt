f0010c34c24545b38b5b8738d48e5a69026f0586
LeeHaiLim <hailim1997@gmail.com>
2023-12-07 20:57:46 +0900

    feat: exoplayer mediasource로 가져오기(#30)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/eventstory/feeddetail/FeedContentsVideoViewHolder.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/eventstory/feeddetail/FeedContentsVideoViewHolder.kt
@@ -1,9 +1,13 @@
 package com.teameetmeet.meetmeet.presentation.eventstory.feeddetail
 
 import android.net.Uri
+import androidx.annotation.OptIn
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DefaultDataSource
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
 import androidx.recyclerview.widget.RecyclerView
 import com.teameetmeet.meetmeet.data.model.Content
 import com.teameetmeet.meetmeet.databinding.ItemFeedContentVideoBinding
@@ -13,7 +17,9 @@ class FeedContentsVideoViewHolder(
     private val contentEventListener: ContentEventListener
 ) : RecyclerView.ViewHolder(binding.root) {
     private val player = ExoPlayer.Builder(itemView.context).build()
+    private val dataSourceFactory = DefaultDataSource.Factory(itemView.context)
 
+    @OptIn(UnstableApi::class)
     fun bind(data: Content) {
         binding.itemEventFeedPv.player = player
 
@@ -21,9 +27,12 @@ class FeedContentsVideoViewHolder(
             contentEventListener.onClick()
         }
 
+        val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(MediaItem.fromUri(Uri.parse(data.path)))
+
         with(player) {
             repeatMode = Player.REPEAT_MODE_ONE
-            setMediaItem(MediaItem.fromUri(Uri.parse(data.path)))
+            setMediaSource(mediaSource)
             prepare()
         }
 
