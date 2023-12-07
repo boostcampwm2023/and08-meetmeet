f072f152f1009336aeaa6e54e6d71f4f0b22b93c
LeeHaiLim <hailim1997@gmail.com>
2023-12-07 19:42:37 +0900

    feat: 피드 콘텐츠 아답터 변경(#30) - 미디어 타입에 따른 뷰홀더 분리

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/eventstory/feeddetail/FeedContentsAdapter.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/eventstory/feeddetail/FeedContentsAdapter.kt
@@ -6,40 +6,53 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.teameetmeet.meetmeet.data.model.Content
-import com.teameetmeet.meetmeet.databinding.ItemFeedContentBinding
+import com.teameetmeet.meetmeet.databinding.ItemFeedContentImageBinding
+import com.teameetmeet.meetmeet.databinding.ItemFeedContentVideoBinding
 
 class FeedContentsAdapter(
-    private val contentClickListener: ContentClickListener
-) : ListAdapter<Content, FeedContentsAdapter.FeedContentsViewHolder>(diffCallback) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedContentsViewHolder {
-        return FeedContentsViewHolder(
-            ItemFeedContentBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            ),
-            contentClickListener
-        )
-    }
+    private val contentEventListener: ContentEventListener
+) : ListAdapter<Content, RecyclerView.ViewHolder>(diffCallback) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIDEO_TYPE -> {
+                FeedContentsVideoViewHolder(
+                    ItemFeedContentVideoBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    ),
+                    contentEventListener
+                )
+            }
 
-    override fun onBindViewHolder(holder: FeedContentsViewHolder, position: Int) {
-        holder.bind(getItem(position))
-    }
-
-    class FeedContentsViewHolder(
-        val binding: ItemFeedContentBinding,
-        private val contentClickListener: ContentClickListener
-    ) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun bind(data: Content) {
-            binding.item = data
-            itemView.setOnClickListener {
-                contentClickListener.onClick()
+            else -> {
+                FeedContentsImageViewHolder(
+                    ItemFeedContentImageBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    ),
+                    contentEventListener
+                )
             }
         }
     }
 
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is FeedContentsImageViewHolder -> holder.bind(getItem(position))
+            is FeedContentsVideoViewHolder -> holder.bind(getItem(position))
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (getItem(position).mimeType.startsWith("video")) VIDEO_TYPE else IMAGE_TYPE
+    }
+
     companion object {
+        private const val IMAGE_TYPE = 0
+        private const val VIDEO_TYPE = 1
+
         private val diffCallback = object : DiffUtil.ItemCallback<Content>() {
             override fun areItemsTheSame(oldItem: Content, newItem: Content): Boolean {
                 return oldItem.id == newItem.id
