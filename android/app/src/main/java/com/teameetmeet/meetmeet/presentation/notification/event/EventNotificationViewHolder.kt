a7d5bedd2436ff9dc03f0433eb4dc81687fd1e66
p-chanmin <pc_m111@naver.com>
2023-12-08 00:24:51 +0900

    feat: 일정 초대 알림 목록 pending 상태 알림 highlight 처리 (#17)

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/notification/event/EventNotificationViewHolder.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/notification/event/EventNotificationViewHolder.kt
@@ -1,6 +1,9 @@
 package com.teameetmeet.meetmeet.presentation.notification.event
 
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.teameetmeet.meetmeet.R
+import com.teameetmeet.meetmeet.data.model.UserStatus
 import com.teameetmeet.meetmeet.data.network.entity.EventInvitationNotification
 import com.teameetmeet.meetmeet.databinding.ItemEventNotificationBinding
 
@@ -12,6 +15,21 @@ class EventNotificationViewHolder(private val binding: ItemEventNotificationBind
         eventNotificationItemClickListener: EventNotificationItemClickListener
     ) {
         binding.item = item
+        when (item.status) {
+            UserStatus.JOIN_STATUS_PENDING -> {
+                itemView.setBackgroundColor(
+                    ContextCompat.getColor(
+                        itemView.context,
+                        R.color.notification_highlight
+                    )
+                )
+            }
+
+            else -> {
+                itemView.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.grey3))
+            }
+        }
+
         itemView.setOnClickListener {
             eventNotificationItemClickListener.onClick(item)
         }
