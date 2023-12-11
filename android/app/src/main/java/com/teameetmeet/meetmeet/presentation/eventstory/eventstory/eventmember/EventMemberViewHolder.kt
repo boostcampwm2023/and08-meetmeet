4efc07ff4ec5e119bde0d302498fd4209994972b
agfalcon <agfalcon12@gmail.com>
2023-12-11 15:50:29 +0900

    refactor: 일정 스토리 이벤트 맴버 더보기 어댑터 뷰홀더 클래스 단일 파일로 정의 및 생성 함수 구현

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/eventstory/eventstory/eventmember/EventMemberViewHolder.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/eventstory/eventstory/eventmember/EventMemberViewHolder.kt
@@ -1,5 +1,7 @@
 package com.teameetmeet.meetmeet.presentation.eventstory.eventstory.eventmember
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.teameetmeet.meetmeet.data.model.UserStatus
 import com.teameetmeet.meetmeet.databinding.ItemEventJoinMemberBinding
@@ -16,6 +18,19 @@ class EventMemberViewHolder(
         }
         itemView.setOnClickListener {
             eventMemberClickListener.onItemViewClick(item)
+        }
+    }
+
+    companion object {
+        fun from(parent: ViewGroup, eventMemberClickListener: EventMemberClickListener) : EventMemberViewHolder {
+            return EventMemberViewHolder(
+                ItemEventJoinMemberBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                ),
+                eventMemberClickListener
+            )
         }
     }
 }
