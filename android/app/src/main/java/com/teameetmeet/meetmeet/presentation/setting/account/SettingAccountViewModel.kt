0c3314811b1fb08215cd89b4405a4285cfd5f691
p-chanmin <pc_m111@naver.com>
2023-12-11 18:38:49 +0900

    feat: SettingAccountFragment exception 분기 처리

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/setting/account/SettingAccountViewModel.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/setting/account/SettingAccountViewModel.kt
@@ -3,6 +3,7 @@ package com.teameetmeet.meetmeet.presentation.setting.account
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.teameetmeet.meetmeet.R
+import com.teameetmeet.meetmeet.data.ExpiredRefreshTokenException
 import com.teameetmeet.meetmeet.data.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.BufferOverflow
@@ -11,6 +12,7 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import java.net.UnknownHostException
 import javax.inject.Inject
 
 @HiltViewModel
@@ -28,11 +30,28 @@ class SettingAccountViewModel @Inject constructor(
         viewModelScope.launch {
             userRepository.deleteUser()
                 .catch {
-                    _event.emit(SettingAccountUiEvent.ShowMessage(R.string.setting_account_delete_fail))
+                    emitExceptionEvent(it, R.string.setting_account_delete_fail)
                 }
                 .collectLatest {
                     _event.emit(SettingAccountUiEvent.NavigateToLoginActivity)
                 }
+        }
+    }
+
+    private suspend fun emitExceptionEvent(e: Throwable, message: Int) {
+        when (e) {
+            is ExpiredRefreshTokenException -> {
+                _event.emit(SettingAccountUiEvent.ShowMessage(R.string.common_message_expired_login))
+                _event.emit(SettingAccountUiEvent.NavigateToLoginActivity)
+            }
+
+            is UnknownHostException -> {
+                _event.emit(SettingAccountUiEvent.ShowMessage(R.string.common_message_no_internet))
+            }
+
+            else -> {
+                _event.emit(SettingAccountUiEvent.ShowMessage(message))
+            }
         }
     }
 }
