329de9a0bc76befabfd1f270d461178184e545b3
LeeHaiLim <hailim1997@gmail.com>
2023-12-15 04:08:47 +0900

    v0.2.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "0.2.0"
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
