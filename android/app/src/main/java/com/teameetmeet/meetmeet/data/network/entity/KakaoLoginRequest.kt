2870cecdba2a47c7cf073fcff9b7198556f62986
p-chanmin <pc_m111@naver.com>
2024-02-26 03:09:19 +0900

    refactor: KakaoLoginRequest Class 변경

--- android/app/src/main/java/com/teameetmeet/meetmeet/data/network/entity/KakaoLoginRequest.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/data/network/entity/KakaoLoginRequest.kt
@@ -4,7 +4,7 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class KakaoLoginRequest (
+class KakaoLoginRequest (
     @Json(name = "kakaoId")
     val kakaoId: String
 )
