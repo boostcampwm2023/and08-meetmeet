ace33bb73b70000e9113c69c8866ee94ec96be78
p-chanmin <pc_m111@naver.com>
2024-02-26 03:11:24 +0900

    build: mockito, corutines-test 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -74,6 +74,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation("org.mockito:mockito-core:5.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     // Activity, Fragment ktx 확장 기능
     implementation("androidx.activity:activity-ktx:1.8.0")
