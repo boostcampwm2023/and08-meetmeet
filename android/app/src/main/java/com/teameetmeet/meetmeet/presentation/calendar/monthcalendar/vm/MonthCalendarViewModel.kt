b039081217434dbb0602a9294a33591cdf5b7b79
LeeHaiLim <hailim1997@gmail.com>
2024-04-11 08:57:23 +0900

    feat: 캘린더 뷰모델 수정 - 리스너 상속 제거 - 이벤트 데이터 저장 - 일정 할당 변경

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/calendar/monthcalendar/vm/MonthCalendarViewModel.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/calendar/monthcalendar/vm/MonthCalendarViewModel.kt
@@ -2,16 +2,13 @@ package com.teameetmeet.meetmeet.presentation.calendar.monthcalendar.vm
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.teameetmeet.meetmeet.presentation.calendar.monthcalendar.CalendarItemClickListener
 import com.teameetmeet.meetmeet.presentation.calendar.monthcalendar.DayClickEvent
 import com.teameetmeet.meetmeet.presentation.model.CalendarItem
-import com.teameetmeet.meetmeet.presentation.model.EventBar
 import com.teameetmeet.meetmeet.presentation.model.EventSimple
 import com.teameetmeet.meetmeet.presentation.util.THROTTLE_DURATION
 import com.teameetmeet.meetmeet.presentation.util.setClickEvent
 import com.teameetmeet.meetmeet.util.date.getLocalDate
 import com.teameetmeet.meetmeet.util.date.toEndLong
-import com.teameetmeet.meetmeet.util.date.toLocalDate
 import com.teameetmeet.meetmeet.util.date.toStartLong
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -20,17 +17,19 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import java.time.LocalDate
 
-abstract class MonthCalendarViewModel : ViewModel(), CalendarItemClickListener {
+abstract class MonthCalendarViewModel : ViewModel() {
 
-    private val _currentDate = MutableStateFlow<CalendarItem>(CalendarItem(getLocalDate()))
+    private val _currentDate = MutableStateFlow(CalendarItem(getLocalDate()))
     val currentDate: StateFlow<CalendarItem> = _currentDate
 
-    private val _daysInMonth = MutableStateFlow<List<CalendarItem>>(
+    private val _daysInMonth = MutableStateFlow(
         currentDate.value.date?.let { CalendarItem.getListFrom(it) } ?: emptyList()
     )
     val daysInMonth: StateFlow<List<CalendarItem>> = _daysInMonth
+
+    private val _monthlyEvents = MutableStateFlow<List<EventSimple>>(emptyList())
+    val monthlyEvents: StateFlow<List<EventSimple>> = _monthlyEvents
 
     private val _dayClickEvent = MutableSharedFlow<CalendarItem>()
 
@@ -47,99 +46,25 @@ abstract class MonthCalendarViewModel : ViewModel(), CalendarItemClickListener {
     abstract fun fetchEvents()
 
     protected fun setDaysInMonth(monthlyEvents: List<EventSimple>) {
-        _daysInMonth.update {
-            allocateEventsPerDay(it, monthlyEvents)
-        }
-        _currentDate.update { prev ->
-            _daysInMonth.value.find { it.date == prev.date } ?: prev
-        }
-    }
-
-    val comparator: (LocalDate) -> Comparator<EventSimple> = { today ->
-        Comparator { event1, event2 ->
-            val endToday1 = event1.endDateTime.toLocalDate() == today
-            val endToday2 = event2.endDateTime.toLocalDate() == today
-
-            if (endToday1 == endToday2) event1.startDateTime.compareTo(event2.startDateTime)
-            else endToday1.compareTo(endToday2)
-        }
+        _monthlyEvents.update { monthlyEvents }
+        _daysInMonth.update { allocateEventsPerDay(it, monthlyEvents) }
+        _currentDate.update { prev -> _daysInMonth.value.find { it.date == prev.date } ?: prev }
     }
 
     private fun allocateEventsPerDay(
         calendarItems: List<CalendarItem>,
         monthlyEvents: List<EventSimple>
     ): List<CalendarItem> {
-        val daysInMonth = mutableListOf<CalendarItem>()
-
-        for (i in calendarItems.indices) {
-            daysInMonth.add(
-                CalendarItem(
-                    date = calendarItems[i].date,
-                    isSelected = calendarItems[i].isSelected
-                )
+        return calendarItems.map { day ->
+            if (day.date == null) day
+            else day.copy(
+                events = monthlyEvents
+                    .filter { event ->
+                        event.startDateTime <= day.date.toEndLong() &&
+                                event.endDateTime >= day.date.toStartLong()
+                    }.sortedBy { it.startDateTime }
             )
-            val today = daysInMonth[i].date
-            today ?: continue
-
-            val todayEvents = monthlyEvents
-                .filter {
-                    it.startDateTime <= today.toEndLong() && it.endDateTime >= today.toStartLong()
-                }.sortedBy { it.startDateTime }
-
-            if (todayEvents.isEmpty()) continue
-
-            val continuity = todayEvents
-                .groupBy { event ->
-                    i % 7 != 0 && today.dayOfMonth != 1 &&
-                            daysInMonth[i - 1].eventBars.any { it?.id == event.id }
-                }
-
-            val eventBars: MutableList<EventBar?> = (0..<5).map { null }.toMutableList()
-
-            continuity[true]?.map { event ->
-                val index = daysInMonth[i - 1].eventBars.indexOfFirst { it?.id == event.id }
-                eventBars[index] = EventBar(
-                    id = event.id,
-                    color = event.color,
-                    isStart = false,
-                    isEnd = i % 7 == 6 || i == calendarItems.lastIndex
-                            || event.endDateTime.toLocalDate() == today
-                )
-            }
-
-            continuity[false]
-                ?.sortedWith(comparator(today))
-                ?.map { event ->
-                    val index = eventBars.indexOf(null)
-                    val eventBar = EventBar(
-                        id = event.id,
-                        color = event.color,
-                        isStart = true,
-                        isEnd = i % 7 == 6 || i == calendarItems.lastIndex
-                                || event.endDateTime.toLocalDate() == today
-                    )
-                    if (index != -1) eventBars[index] = eventBar else eventBars.add(eventBar)
-                }
-
-            if (eventBars.size > 5) {
-                eventBars[4] = EventBar(
-                    id = -1, isStart = true, isEnd = true, hiddenCount = eventBars.size - 4
-                )
-                if (i % 7 != 0 && today.dayOfMonth != 1) {
-                    daysInMonth[i - 1] = daysInMonth[i - 1].copy(
-                        eventBars = daysInMonth[i - 1].eventBars
-                            .mapIndexed { index, eventBar ->
-                                if (index == 4) eventBar?.copy(isEnd = true)
-                                else eventBar
-                            }
-                    )
-                }
-            }
-
-            daysInMonth[i] =
-                daysInMonth[i].copy(events = todayEvents, eventBars = eventBars.take(5))
         }
-        return daysInMonth
     }
 
     fun moveMonth(offset: Long) {
@@ -152,23 +77,25 @@ abstract class MonthCalendarViewModel : ViewModel(), CalendarItemClickListener {
         fetchEvents()
     }
 
-    override fun onItemClick(calendarItem: CalendarItem) {
-        calendarItem.date ?: return
-        if (calendarItem.events.isNotEmpty()) {
+    fun selectDay(index: Int) {
+        if (index >= daysInMonth.value.size) return
+        val selectedDate = daysInMonth.value[index]
+        selectedDate.date ?: return
+        if (selectedDate.events.isNotEmpty()) {
             viewModelScope.launch {
-                _dayClickEvent.emit(calendarItem)
+                _dayClickEvent.emit(selectedDate)
             }
         }
-        if (currentDate.value.date == calendarItem.date) return
+        if (currentDate.value.date == selectedDate.date) return
         _daysInMonth.update { list ->
             list.map {
                 when (it.date) {
-                    calendarItem.date -> it.copy(isSelected = true)
+                    selectedDate.date -> it.copy(isSelected = true)
                     else -> it.copy(isSelected = false)
                 }
             }
         }
-        _currentDate.update { calendarItem }
+        _currentDate.update { selectedDate }
     }
 
     private fun setDayClickRequestFlow() {
