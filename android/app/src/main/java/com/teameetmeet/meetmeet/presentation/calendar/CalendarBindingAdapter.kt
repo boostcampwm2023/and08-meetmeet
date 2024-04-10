2b5161ec6072ffbd67018aec969ab1012eabaf36
LeeHaiLim <hailim1997@gmail.com>
2024-04-11 08:58:30 +0900

    feat: 캘린더 레이아웃에 커스텀뷰 추가

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/calendar/CalendarBindingAdapter.kt
+++ android/app/src/main/java/com/teameetmeet/meetmeet/presentation/calendar/CalendarBindingAdapter.kt
@@ -4,6 +4,9 @@ import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.google.android.material.badge.BadgeDrawable
+import com.teameetmeet.meetmeet.presentation.calendar.monthcalendar.MonthCalendarView
+import com.teameetmeet.meetmeet.presentation.model.CalendarItem
+import com.teameetmeet.meetmeet.presentation.model.EventSimple
 import com.teameetmeet.meetmeet.util.date.DateTimeFormat
 import java.time.LocalDate
 
@@ -16,4 +19,14 @@ fun TextView.bindLocalDate(localDate: LocalDate) {
 fun FrameLayout.bindBadgeCount(count: Int, drawable: BadgeDrawable) {
     drawable.number = count
     drawable.isVisible = count > 0
+}
+
+@BindingAdapter("days_in_month")
+fun MonthCalendarView.bindDaysInMonth(calendarItems: List<CalendarItem>) {
+    setDaysInMonth(calendarItems)
+}
+
+@BindingAdapter("monthly_events")
+fun MonthCalendarView.bindMonthlyEvents(events: List<EventSimple>) {
+    setEvents(events)
 }
