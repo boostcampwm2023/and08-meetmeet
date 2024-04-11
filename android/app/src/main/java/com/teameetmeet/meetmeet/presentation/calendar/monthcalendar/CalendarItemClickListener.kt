61c46c0d982b2d7fb11d3a6e40043ef01e766592
LeeHaiLim <hailim1997@gmail.com>
2024-04-11 14:09:08 +0900

    chore: 불필요한 파일 삭제

--- android/app/src/main/java/com/teameetmeet/meetmeet/presentation/calendar/monthcalendar/CalendarItemClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.teameetmeet.meetmeet.presentation.calendar.monthcalendar
-
-import com.teameetmeet.meetmeet.presentation.model.CalendarItem
-
-interface CalendarItemClickListener {
-    fun onItemClick(calendarItem: CalendarItem)
-}
